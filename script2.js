a0faee3af96af33ccd0fa22a5fbb80de00989e7a
SempaiOchilov <139633008+SempaiOchilov@users.noreply.github.com>
2023-10-31 17:22:03 +0500

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,16 @@
+let cars = [{
+    model: "ВАЗ 2106",
+    company: "ВАЗ", 
+    color: "dry asphalt",
+    engine: 1.5 ,
+    price: 3000,
+    beaten: true,
+    year: 1995,
+    mileage: 100000,
+    tech_passport: {
+        date: 1996,
+        exp_date: 2026,
+    }
+}
+]
+
